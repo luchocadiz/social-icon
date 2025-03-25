3d20694d9498c7a488589744890c7b45bdb1595b
Luis Cadiz <luiscadiz1504@gmail.com>
2025-03-25 12:03:30 -0300

    se crean los iconos

--- kui-social-icon.js
+++ kui-social-icon.js
@@ -1,16 +1,57 @@
 import { LitElement, html, css } from 'lit';
+import { icons } from './icons';
 
-export class SocialIcon extends LitElement {
-    static styles = [
-        css`
-            :host {
-                display: block;
-            }
-        `
-    ];
+/**
+ * 
+ * # <social-icon>
+ * 
+ * Web component to create social icons, based on LitElement.
+ * 
+ * The "icon" property is used to set the social network to display
+ * 
+ */
 
-    render() {
-        return html`Hello WebComponent.org`;
-    }
+export class KuiSocialIcon extends LitElement {
+
+    static get properties() {
+        return { 
+          icon: { type: String }
+        };
+      }
+    
+      constructor() {
+        super();
+        this.icon = 'facebook';
+      }
+    
+      static get styles() {
+        return css`
+          :host[hidden] { display: none; }
+          :host { display: inline-block; }
+          path {
+            fill: var(--kui-social-icon-color, #888);
+          }
+          path[fill="none"] {
+            fill: transparent;
+          }
+          svg {
+            width: var(--kui-social-icon-size, 24px);
+            height: var(--kui-social-icon-size, 24px);
+          }
+          div { display: flex;}
+        `;
+      }
+    
+      render() {
+        return html`
+        <div>
+          ${this._getIcon(this.icon)}
+        </div>
+        `;
+      }
+    
+      _getIcon(icon) {
+        return icons[icon];
+      }
 }
-customElements.define('social-icon', SocialIcon);
+customElements.define('kui-social-icon', KuiSocialIcon);
